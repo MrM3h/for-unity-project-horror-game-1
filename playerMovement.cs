6dce68c4dc216f09a335b454e47e0ecf0dc096aa
MrM3h <150963592+MrM3h@users.noreply.github.com>
2024-03-27 18:15:56 -0400

    Add files via upload

---
+++ playerMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float speed;
+    public float jump;
+
+    private float move;
+
+    public Rigidbody2D rb;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        {
+
+          
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+                move = Input.GetAxis("Horizontal");
+
+                rb.velocity = new vector2(speed + move, rb.velocity.y);
+
+                if (Input.GetButtonDown("Jump"))
+                {
+                    rb.AssForce(new vector2(rb.velocity.x, jump));
+
+
+
+
+
+
+
+
+        
+    }
+}
